efda5aa0c7c0de5488921a88a2befb229e740eac
chunloklo <clo42@gatech.edu>
2018-10-15 13:40:32 -0400

    Initial iteration on tower rotation mechanics, no max angle have been implemented yet

---
+++ Assets/Scripts/Tower/TowerRotater.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerRotater : MonoBehaviour {
+
+    public Transform[] pieces;
+    public float[] ratio;
+    Quaternion lastRotation;
+	// Use this for initialization
+	void Start () {
+        lastRotation = transform.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Quaternion delta = Quaternion.Inverse(lastRotation) * transform.localRotation;
+        Debug.Log("Delta: " + (delta));
+        lastRotation = transform.localRotation;
+
+        for (int i = 0; i < pieces.Length; i++)
+        {
+            pieces[i].transform.localRotation *= delta;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
+}
