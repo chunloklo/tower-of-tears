a6d5712273321dcdcc1c289ee299cc4b2b5e6a00
gtsli <gtsli469@gmail.com>
2018-10-16 17:42:02 -0400

    added colored torches

---
+++ Assets/Scripts/torch/TorchSwing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchSwing : MonoBehaviour {
+
+	Animator anim;
+
+	void Start() {
+		anim = GetComponent<Animator> ();
+		anim.SetTrigger("TorchSpin");
+	}
+
+//	void OnTriggerEnter(Collider c) {
+//		anim.SetTrigger("TorchSpin");
+//	}
+
+//	void OnTriggerExit(Collider c) {
+//		anim.Play("Idle");
+//	}
+}
