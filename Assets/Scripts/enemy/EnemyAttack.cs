bbc579e04a73889e7c872940702ea028f9952c30
gtsli <gtsli469@gmail.com>
2018-10-20 15:22:45 -0400

    added ai boss scenee with basic boss and dummy player

---
+++ Assets/Scripts/enemy/EnemyAttack.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour
+{
+	public float timeBetweenAttacks = 0.5f;
+	public int attackDamage = 10;
+
+
+	Animator anim;
+	GameObject player;
+	PlayerHealth playerHealth;
+	EnemyHealth enemyHealth;
+	bool playerInRange;
+	float timer;
+
+
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag ("MovingWaypointCube");
+		playerHealth = player.GetComponent <PlayerHealth> ();
+		enemyHealth = GetComponent<EnemyHealth>();
+		anim = GetComponent <Animator> ();
+	}
+
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject == player)
+		{
+			playerInRange = true;
+		}
+	}
+
+
+	void OnTriggerExit (Collider other)
+	{
+		if(other.gameObject == player)
+		{
+			playerInRange = false;
+		}
+	}
+
+
+	void Update ()
+	{
+		timer += Time.deltaTime;
+
+		if(timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
+		{
+			Attack ();
+		}
+
+		if(playerHealth.currentHealth <= 0)
+		{
+			Destroy (player);
+			Debug.Log ("PLAYER DEAD");
+		}
+	}
+
+
+	void Attack ()
+	{
+		timer = 0f;
+
+		if(playerHealth.currentHealth > 0)
+		{
+			playerHealth.TakeDamage (attackDamage);
+		}
+	}
+}
