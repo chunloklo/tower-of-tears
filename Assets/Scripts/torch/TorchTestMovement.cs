0f56454bc37a0287a69ceef63d5840fdeeb3200f
leoniereif <lreif3@gatech.edu>
2018-10-22 11:56:03 -0400

    added some lamp trigger options

---
+++ Assets/Scripts/torch/TorchTestMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchTestMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(2f*Input.GetAxis("Horizontal") * Time.deltaTime,0f,2f*Input.GetAxis("Vertical") * Time.deltaTime);
+	}
+}
