a2ea888983bbb52fe3a961917d711f0fd00741a6
leoniereif <lreif3@gatech.edu>
2018-10-26 11:31:31 -0400

    added trap trigger and tried to get a fog shader to work

--- Assets/Scripts/lamps/TurnOnOffController.cs
+++ Assets/Scripts/lamps/TurnOnOffController.cs
@@ -7,21 +7,30 @@ public class TurnOnOffController : MonoBehaviour {
     Light wallLight;
     private ParticleSystem.EmissionModule flame;
 
+    GameObject trap;
     private void Awake()
     {
         wallLight = this.gameObject.transform.GetChild(0).GetComponent<Light>();
         flame = this.gameObject.transform.GetChild(1).GetComponent<ParticleSystem>().emission;
+
+
+        trap = GameObject.FindWithTag("Trap");
     }
 
     void OnTriggerEnter(Collider c) {
+        print(c);
         // turn on point light and flame particles
         if (!wallLight.enabled) {
             wallLight.enabled = true;
             flame.enabled = true;
+
+            trap.GetComponent<Animation>().Play("Anim_SawTrap01_Play");
         } 
     }
 
-    void OnTriggerExit(Collider c) {
+    void OnTriggerExit(Collider c)
+    {   
+        print(c);
         if (wallLight.enabled)
         {
             wallLight.enabled = false;
