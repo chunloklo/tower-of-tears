473adcdbe75af11830bb690eea08f133ae1f915b
Bryan <bkim360@gatech.edu>
2018-11-20 20:39:31 -0500

    spear

---
+++ Assets/Scripts/Trap/SpearController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearController : MonoBehaviour {
+
+
+    private float max = 0f;
+    private float min = 0f;
+
+	void Start () {
+        max = transform.position.y + 3;
+        min = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time * 2, max - min) + min, transform.position.z);
+    }
+}
