ba0fd64860980044a0ffdec43e50a42a0e663d0e
stephaniebaione <stephaniebaione@gatech.edu>
2018-12-09 17:12:08 -0500

    added sound effect to boss death

---
+++ Assets/Scripts/Cutscenes/CutsceneMusic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class CutsceneMusic : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public CutsceneTimer ct;
+    bool played;
+
+    // Use this for initialization
+    void Start () {
+        played = false;
+        ct = ct.GetComponent<CutsceneTimer>();
+        audioSource = GetComponent<AudioSource>();
+        Debug.Log("working");
+        //audioSource.Play(0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (ct.GetTime() > 14 && !played)
+        {
+            audioSource.Play(0);
+            played = true;
+        }
+	}
+}
