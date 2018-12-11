e665ab20ee42901371dcf1a7759ac517541b543b
Bryan <bkim360@gatech.edu>
2018-12-11 02:39:56 -0500

    spear

--- Assets/Scripts/Trap/SpearController.cs
+++ Assets/Scripts/Trap/SpearController.cs
@@ -4,17 +4,58 @@ using UnityEngine;
 
 public class SpearController : MonoBehaviour {
 
-
     private float max = 0f;
     private float min = 0f;
 
+    public bool North = false;
+    public bool East = false;
+    public bool South = false;
+    public bool West = false;
+
 	void Start () {
-        max = transform.position.y + 3;
-        min = transform.position.y;
+
+        if (North == true)
+        {
+            max = transform.position.z + 3;
+            min = transform.position.z;
+        }
+        if (East == true)
+        {
+            max = transform.position.x + 3;
+            min = transform.position.x;
+        }
+        if (South == true)
+        {
+            max = transform.position.z;
+            min = transform.position.z - 3;
+        }
+        if (West == true)
+        {
+            max = transform.position.x;
+            min = transform.position.x - 3;
+        }
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time * 2, max - min) + min, transform.position.z);
+
+        if(North == true)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.PingPong(Time.time * 2, max - min) + min);
+        }
+        if (East == true)
+        {
+            transform.position = new Vector3(Mathf.PingPong(Time.time * 2, max - min) + min, transform.position.y, transform.position.z);
+        }
+        if (South == true)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.PingPong(Time.time * 2, max - min) + min);
+        }
+        if (West == true)
+        {
+            transform.position = new Vector3(Mathf.PingPong(Time.time * 2, max - min) + min, transform.position.y, transform.position.z);
+        }
+        
     }
 }
