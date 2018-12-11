f1f01dd82b65421a952660ca1b0eda99f0169cb1
Leonie Reif <lreif3@gatech.edu>
2018-12-11 14:19:09 -0500

    updated menus with snow

---
+++ Assets/Scripts/Util/SnowMenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowMenuController : MonoBehaviour {
+
+    public ParticleSystem snow;
+
+    // Use this for initialization
+    void Start () {
+        snow = GetComponent<ParticleSystem>();
+        if (snow != null) {
+            var emission = snow.emission;
+            if (CrossSceneInformation.santaHat == true)
+            {
+                emission.enabled = true;
+            }
+            else
+            {
+                emission.enabled = false;
+            }
+        }
+
+    }
+}
