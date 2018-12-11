1e797735f14b510dc8d67796ea87466e69de2284
stephaniebaione <stephaniebaione@gatech.edu>
2018-12-11 17:04:52 -0500

    created boss cutscene intro

---
+++ Assets/Scripts/EventSystem/TransitionFromBossCutsceneToGame.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionFromBossCutsceneToGame : MonoBehaviour
+{
+    public Animator animator;
+    public CutsceneTimerSnow ct;
+
+    public void Start()
+    {
+        ct = GetComponent<CutsceneTimerSnow>();
+    }
+
+    public void Update()
+    {
+        if (ct != null && ct.GetTime() > 90)
+        {
+            Transition();
+        }
+    }
+
+    public void Transition()
+    {
+        if (animator != null)
+        {
+            animator.SetTrigger("FadeOut");
+        }
+
+        Time.timeScale = 1;
+        StartCoroutine(waitThenTransition());
+
+    }
+
+    IEnumerator waitThenTransition()
+    {
+        yield return new WaitForSeconds(0.7F);
+        SceneManager.LoadScene("AlphaDemoBoss");
+        Debug.Log("waitning for transition");
+
+    }
+}
