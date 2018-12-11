c7e85d18d1be65aacbf57a697ce8fc72bc9a25de
chunloklo <clo42@gatech.edu>
2018-12-11 18:29:54 -0500

    override footstep changes on levels to add rotation sounds

---
+++ Assets/Scripts/Tower/RotationSound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class RotationSound : MonoBehaviour {
+
+
+    private AudioSource audioSource;
+    private Quaternion rotation;
+    private Vector3 position;
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        rotation = transform.rotation;
+        position = transform.position;
+        audioSource.loop = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (rotation == transform.rotation && position == transform.position)
+        {
+            audioSource.mute = true;
+        } else
+        {
+            audioSource.mute = false;
+        }
+
+
+        rotation = transform.rotation;
+        position = transform.position;
+    }
+}
